67af023a1d3842b554992b1d6ff08a0438d3a6d8
downtownbrown19 <43194191+downtownbrown19@users.noreply.github.com>
2019-02-22 20:40:42 -0500

    camelot table detection with hard coded table borders

--- redeem.py
+++ redeem.py
@@ -3,7 +3,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 tables = camelot.read_pdf('1872_recognized.pdf',pages='1-38', column_tol=2, flavor='stream', table_areas=['35,555,356,374','35,369,356,206','35,196,356,26'])
-
 #tables = camelot.read_pdf('doc.pdf', flavor='stream')
 #camelot.plot(tables[0], kind='text')
 #plt.show()
